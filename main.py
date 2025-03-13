80e2d4f1c4e859b1ef40eb5b20b05d0dce374316
john kennedy <kennedysarmiento33@gmail.com>
2025-03-13 23:28:04 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+places = []
+
+while True:
+    place = input("Enter a place (or 'q' to quit): ")
+    if place == 'q':
+        break
+    places.append(place)
+
+places_json = json.dumps(places)
+
+print("All places entered:")
+for place in places:
+    print(place
